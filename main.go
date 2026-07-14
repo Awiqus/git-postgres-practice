c1075f7e86a8f3cee2b434aaab72f5987c58b6d0
Vladimir Valiev <valiev.vladimir.v@gamil.com>
2026-07-14 16:56:45 +0300

    add simpleconnection

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"nilchan/feature1"
 	"nilchan/feature2"
+	simpleconnection "nilchan/feature_postgres/simple_connection"
 )
 
 func main() {
 	fmt.Println("Hello Git!")
 	feature1.Feature1()
 	feature2.Feature2()
+	simpleconnection.CheckConnection()
 }
