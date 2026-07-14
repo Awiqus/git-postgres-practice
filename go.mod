2038e2cbb7d5095c006f8b8d7b11871f21ce4c24
Vladimir Valiev <valiev.vladimir.v@gamil.com>
2026-07-14 13:37:07 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module nilchan
+
+go 1.26.3
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"nilchan/feature1"
+	"nilchan/feature2"
+)
+
+func main() {
+	fmt.Println("Hello Git!")
+	feature1.Feature1()
+	feature2.Feature2()
+}
